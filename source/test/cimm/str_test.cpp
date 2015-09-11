0196688c2aa1e9947c7ceeef367835f0b617453c
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-09-11 18:43:17 +0200

    refactored str_test

--- source/test/cimm/str_test.cpp
+++ source/test/cimm/str_test.cpp
@@ -11,9 +11,9 @@ TEST(str_test, should_just_return_the_given_string)
 
 TEST(str_test, should_convert_the_given_integer_to_a_string)
 {
-    ASSERT_EQ(string("0"), str(expression(integer(0))));
-    ASSERT_EQ(string("31902"), str(expression(integer(31902))));
-    ASSERT_EQ(string("-78"), str(expression(integer(-78))));
+    EXPECT_EQ(string("0"), str(expression(integer(0))));
+    EXPECT_EQ(string("31902"), str(expression(integer(31902))));
+    EXPECT_EQ(string("-78"), str(expression(integer(-78))));
 }
 
 }
