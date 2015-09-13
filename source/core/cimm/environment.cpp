028223e9fbb3051befbf6c0e3da0ac599ed50320
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-09-14 00:01:47 +0200

    refactored define_native_function

--- source/core/cimm/environment.cpp
+++ source/core/cimm/environment.cpp
@@ -5,7 +5,7 @@ namespace cimm
 
 auto define_native_function(environment& env, string name, native_function fn) -> void
 {
-    env.functions[name] = fn;
+    env.functions.emplace(std::move(name), std::move(fn));
 }
 
 namespace
