e47d86e960e9463af0269ef6e247145639a8e03c
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-09-22 21:36:26 +0200

    fn_test should_evaluate_functions_with_one_argument

--- source/test/cimm/fn_test.cpp
+++ source/test/cimm/fn_test.cpp
@@ -24,6 +24,7 @@ TEST_F(fn_test, should_evaluate_functions_with_one_argument)
     EXPECT_EQ(integer(1), evaluate_parsed("((fn [x] (- x 1)) 2)"));
     EXPECT_EQ(integer(3), evaluate_parsed("((fn [x] (+ 1 x)) 2)"));
     EXPECT_EQ(integer(7), evaluate_parsed("((fn [x] (+ x x 1 x)) 2)"));
+    EXPECT_EQ(integer(7), evaluate_parsed("((fn [f] (f 3 4)) +)"));
 }
 
 }
