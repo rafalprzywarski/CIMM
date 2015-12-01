fddae40fc2c34aadf614e49e97fbaa432addbc06
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-01 15:51:51 +0100

    ostream operators for tests use pr_str

--- source/test/cimm/expression_ostream.hpp
+++ source/test/cimm/expression_ostream.hpp
@@ -7,27 +7,27 @@ namespace cimm
 
 inline auto operator<<(std::ostream& os, const nil_type& e) -> std::ostream&
 {
-    return os << str(e);
+    return os << pr_str(e);
 }
 
 inline auto operator<<(std::ostream& os, const list& l) -> std::ostream&
 {
-    return os << str(l);
+    return os << pr_str(l);
 }
 
 inline auto operator<<(std::ostream& os, const vector& v) -> std::ostream&
 {
-    return os << str(v);
+    return os << pr_str(v);
 }
 
 inline auto operator<<(std::ostream& os, const symbol& s) -> std::ostream&
 {
-    return os << str(s);
+    return os << pr_str(s);
 }
 
 inline auto operator<<(std::ostream& os, const expression& e) -> std::ostream&
 {
-    return os << str(e);
+    return os << pr_str(e);
 }
 
 }
