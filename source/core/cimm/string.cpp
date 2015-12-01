4cd023cb048081bf4fff51ba3d689b57c4c17a55
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-01 22:07:07 +0100

    refactored pr_str

--- source/core/cimm/string.cpp
+++ source/core/cimm/string.cpp
@@ -10,22 +10,23 @@ namespace
 
 std::array<std::pair<char, const char *>, 3> escaped = { { { '\n', "\\n" }, { '\"', "\\\"" }, { '\\', "\\\\" } } };
 
+void append_char(char c, string& printed)
+{
+    auto found = std::find_if(begin(escaped), end(escaped), [c](auto& e) { return e.first == c; });
+    if (found != end(escaped))
+        printed += found->second;
+    else
+        printed += c;
+}
+
 }
 
 auto pr_str(const string& s) -> string
 {
     string printed = "\"";
-
     for (auto c : s)
-    {
-        auto found = std::find_if(begin(escaped), end(escaped), [c](auto& e) { return e.first == c; });
-        if (found != end(escaped))
-            printed += found->second;
-        else
-            printed += c;
-    }
-    printed += "\"";
-    return printed;
+        append_char(c, printed);
+    return printed += "\"";
 }
 
 }
