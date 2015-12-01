ce5586a913dfd95d2b987883dbd685bb092d1cee
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-01 13:00:45 +0100

    refactored pr_str

--- source/core/cimm/str.cpp
+++ source/core/cimm/str.cpp
@@ -50,29 +50,9 @@ auto str(const expression& e) -> string
     return apply(to_string(), e);
 }
 
-namespace
-{
-
-struct pr_to_string : expression::visitor<string>
-{
-    string operator()(const nil_type& nil) const { return pr_str(nil); }
-    string operator()(const symbol& s) const { return pr_str(s); }
-    string operator()(const keyword& k) const { return pr_str(k); }
-    string operator()(const string& s) const { return pr_str(s); }
-    string operator()(const integer& v) const { return pr_str(v); }
-    string operator()(const boolean& b) const { return pr_str(b); }
-    string operator()(const list& l) const { return pr_str(l); }
-    string operator()(const vector& v) const { return pr_str(v); }
-    string operator()(const native_function& f) const { return pr_str(f); }
-    string operator()(const function& f) const { return pr_str(f); }
-    string operator()(const error& e) const { return pr_str(e); }
-};
-
-}
-
 auto pr_str(const expression& e) -> string
 {
-    return apply(pr_to_string(), e);
+    return apply([](auto& e) { return pr_str(e); }, e);
 }
 
 }
