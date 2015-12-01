a54a9111db59e4d0fa23bc0573fcfb8f8f16a21b
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-01 12:22:44 +0100

    str uses pr_str for everything except strings

--- source/core/cimm/str.cpp
+++ source/core/cimm/str.cpp
@@ -38,23 +38,20 @@ string join(char open, list const& l, char close)
 
 }
 
+namespace
+{
+
+struct to_string : expression::visitor<string>
+{
+    template <typename expression_type>
+    string operator()(const expression_type& e) const { return pr_str(e); }
+    string operator()(const string& s) const { return s; }
+};
+
+}
+
 auto str(const expression& e) -> string
 {
-    struct to_string : expression::visitor<string>
-    {
-        string operator()(const nil_type& ) const { return "nil"; }
-        string operator()(const symbol& s) const { return str(s); }
-        string operator()(const keyword& k) const { return str(k); }
-        string operator()(const string& s) const { return s; }
-        string operator()(const integer& v) const { return std::to_string(v); }
-        string operator()(const boolean& b) const { return b ? "true" : "false"; }
-        string operator()(const list& l) const { return join('(', l, ')'); }
-        string operator()(const vector& v) const { return join('[', v, ']'); }
-        string operator()(const native_function& ) const { return "fn"; }
-        string operator()(const function& f) const { return "fn"; }
-        string operator()(const error& e) const { return str(e); }
-    };
-
     return apply(to_string(), e);
 }
 
@@ -63,10 +60,17 @@ namespace
 
 struct pr_to_string : expression::visitor<string>
 {
+    string operator()(const nil_type& ) const { return "nil"; }
+    string operator()(const symbol& s) const { return str(s); }
+    string operator()(const keyword& k) const { return str(k); }
     string operator()(const string& s) const { return '\"' + s + '\"'; }
-
-    template <typename expression_type>
-    string operator()(const expression_type& e) const { return str(e); }
+    string operator()(const integer& v) const { return std::to_string(v); }
+    string operator()(const boolean& b) const { return b ? "true" : "false"; }
+    string operator()(const list& l) const { return join('(', l, ')'); }
+    string operator()(const vector& v) const { return join('[', v, ']'); }
+    string operator()(const native_function& ) const { return "fn"; }
+    string operator()(const function& f) const { return "fn"; }
+    string operator()(const error& e) const { return str(e); }
 };
 
 }
