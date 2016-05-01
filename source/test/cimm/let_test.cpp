74975264aaf888a559c0385d88973d4cc47e97f0
Rafal Przywarski <rafal.przywarski@gmail.com>
2016-05-01 19:16:18 +0200

    let_test should_allow_for_no_bindings

--- source/test/cimm/let_test.cpp
+++ source/test/cimm/let_test.cpp
@@ -11,6 +11,11 @@ TEST_F(let_test, should_define_local_constants)
     EXPECT_EQ(evaluate_parsed("'(1 2 3)"), evaluate_parsed("(let [a 1 b 2 c 3] (list a b c))"));
 }
 
+TEST_F(let_test, should_allow_for_no_bindings)
+{
+    EXPECT_EQ(integer{7}, evaluate_parsed("(let [] 7)"));
+}
+
 TEST_F(let_test, should_fail_for_odd_number_of_forms)
 {
     assert_evaluation_error<let_forms_error>("let requires an even number of forms in binding vector", "(let [x] 1)");
